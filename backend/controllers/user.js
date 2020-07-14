c3d4524f403db55d2e0343d3d66049f088b768c8
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-07-14 18:18:01 +0100

    implemented masking functionality for email before to be stored in database

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt =  require('bcrypt');
+const maskData = require('../node_modules/maskdata');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
@@ -6,9 +7,10 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10).then(
       (hash) => {
         const user = new User({
-          email: req.body.email,
+          email: maskData.maskEmail2(req.body.email),
           password: hash
         });
+        console.log(user)
         user.save().then(
           () => {
             res.status(201).json({
@@ -26,8 +28,9 @@ exports.signup = (req, res, next) => {
     );
   };
 
+
   exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }).then(
+    User.findOne({ email: maskData.maskEmail2(req.body.email) }).then(
       (user) => {
         if (!user) {
           return res.status(401).json({
